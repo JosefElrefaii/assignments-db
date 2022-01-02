28a342b94449d5c4dac4f7d49ee663e609040768
Josef Elrefai <josef@technocreatives.com>
2022-01-02 21:32:36 +0100

    Fix assignments qury

--- src/assignments.rs
+++ src/assignments.rs
@@ -3,7 +3,10 @@ use sqlx::PgConnection;
 #[derive(Debug, Clone)]
 pub struct Assignment {
   id: i32,
-  name: String
+  name: String,
+  role_name: String,
+  description: String,
+  git_url: String
 }
 
 pub async fn assignments(conn: &mut PgConnection) -> Result<Vec<Assignment>, sqlx::Error> {
